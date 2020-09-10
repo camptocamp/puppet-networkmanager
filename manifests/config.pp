997159bad4ec7f44453790ca0185c1a60fe647c5
Bram Vogelaar <bram.vogelaar@hotpotatoes.nl>
2020-09-10 11:43:35 +0200

    removing legacy calls

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,4 @@
 class networkmanager::config {
-  validate_hash($::networkmanager::openconnect_connections)
-  validate_hash($::networkmanager::openvpn_connections)
-  validate_hash($::networkmanager::wifi_connections)
-
   create_resources(
     'networkmanager::openconnect',
     $::networkmanager::openconnect_connections
